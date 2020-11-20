cb0ed4767dca6b129bfa19e70dee662d4cdb36db
ofk <ofkjpn+github@gmail.com>
2020-11-21 00:56:16 +0900

    Implement clearCache

--- src/background.ts
+++ src/background.ts
@@ -1,19 +1,10 @@
 import { storage } from './utils/storage';
 
-const DEFAULT_OPTIONS = {
-  enabled: true,
-  color: 'blue',
-};
-
 chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
   // eslint-disable-next-line default-case
   switch (message.action) {
-    case `${process.env.PACKAGE_NAME}.getOptions`:
-      sendResponse({ options: storage.get('options') || { ...DEFAULT_OPTIONS } });
-      break;
-
-    case `${process.env.PACKAGE_NAME}.setOptions`:
-      storage.set('options', message.options);
+    case `${process.env.PACKAGE_NAME}.clearCache`:
+      storage.clear('artworks');
       sendResponse({});
       break;
   }
--- src/options.tsx
+++ src/options.tsx
@@ -1,59 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { Grid, FormControlLabel, Checkbox, TextField } from '@material-ui/core';
+import { Grid, Button } from '@material-ui/core';
 
 import { Base } from './utils/Base';
 
 const App: React.FC = () => {
-  const [options, setOptions] = useState<{ enabled: boolean; color: string } | null>(null);
-
-  useEffect(() => {
-    chrome.runtime.sendMessage({ action: `${process.env.PACKAGE_NAME}.getOptions` }, (message) => {
-      setOptions(message.options);
-    });
-  }, []);
-
-  const updateOptions = (nextOptions: { enabled: boolean; color: string }): void => {
-    setOptions(nextOptions);
-    chrome.runtime.sendMessage({
-      action: `${process.env.PACKAGE_NAME}.setOptions`,
-      options: nextOptions,
-    });
-  };
-
   return (
     <Base>
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          {options ? (
-            <>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={options.enabled}
-                    onChange={(event): void => {
-                      updateOptions({ ...options, enabled: event.target.checked });
-                    }}
-                    name="enabled"
-                    color="primary"
-                  />
-                }
-                label="Enabled"
-              />
-              <TextField
-                id="color"
-                name="color"
-                label="Color"
-                fullWidth
-                margin="dense"
-                size="small"
-                value={options.color}
-                onChange={(event): void => {
-                  updateOptions({ ...options, color: event.target.value });
-                }}
-              />
-            </>
-          ) : null}
+          <Button
+            variant="outlined"
+            onClick={(): void => {
+              chrome.runtime.sendMessage({ action: `${process.env.PACKAGE_NAME}.clearCache` });
+            }}
+          >
+            Clear cache
+          </Button>
         </Grid>
       </Grid>
     </Base>
